811da16a1e35d3a30be003586b1301956c447a8f
ilariarm <76061071+ilariarm@users.noreply.github.com>
2021-03-03 14:37:34 +0100

    Inserted // eslint-disable-line for cleaning the warning in WeatherApp.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import WeatherApp from "./WeatherApp";
+
+// eslint-disable-line
 
 function App() {
   return (
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; // eslint-disable-line
 import axios from "axios";
 
 export default function WeatherApp(props) {
